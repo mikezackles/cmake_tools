58e833c9450609c48660caa62e35887784a9bd1f
Zachary Michaels <mikezackles@gmail.com>
2016-07-09 14:46:44 -0400

    Add default_build_type function

---
+++ default_build_type.cmake
@@ -0,0 +1,7 @@
+function(DEFAULT_BUILD_TYPE default)
+  if (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    message(STATUS "Setting build type to '${default}' as none was specified")
+    set(CMAKE_BUILD_TYPE "${default}" CACHE STRING "Choose a build type" FORCE)
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+  endif()
+endfunction()
