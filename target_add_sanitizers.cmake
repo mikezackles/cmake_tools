f59a0a8f5cc815ee61ca4f3edd7da0de6307ed4c
Zachary Michaels <mikezackles@gmail.com>
2016-07-11 11:59:38 -0400

    Fix gcc sanitizers and clarify settings

--- target_add_sanitizers.cmake
+++ target_add_sanitizers.cmake
@@ -1,8 +1,8 @@
 include(check_compiler)
 
 function(TARGET_ADD_SANITIZERS target)
-  set(use_sanitizers true CACHE BOOL "Use sanitizers if available (requires libubsan for gcc)")
-  if (use_sanitizers)
+  set(USE_SANITIZERS_FOR_DEBUG true CACHE BOOL "Use sanitizers if available on debug builds (requires libubsan for gcc)")
+  if (USE_SANITIZERS_FOR_DEBUG)
     check_compiler()
     if (is_clang)
       target_compile_options("${target}" PRIVATE
@@ -20,6 +20,7 @@ function(TARGET_ADD_SANITIZERS target)
         $<$<CONFIG:Debug>:-fno-omit-frame-pointer>
         $<$<CONFIG:Debug>:-fno-optimize-sibling-calls>)
       target_link_libraries("${target}" PRIVATE
+        $<$<CONFIG:Debug>:-fsanitize=address>
         $<$<CONFIG:Debug>:-lubsan>)
     endif()
   endif()
