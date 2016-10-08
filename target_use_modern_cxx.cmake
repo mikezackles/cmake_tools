b5edfa07c81d4f857921e5c70bb5ae511364a0fc
Zachary Michaels <mikezackles@gmail.com>
2016-10-08 14:13:30 -0700

    Always turn off GNU extensions

--- target_use_modern_cxx.cmake
+++ target_use_modern_cxx.cmake
@@ -1,6 +1,7 @@
 function(TARGET_USE_MODERN_CXX target)
   cmake_parse_arguments(PARSED "" "TYPE" "RELEASE_WHITELIST" ${ARGN})
 
+  set(CMAKE_CXX_EXTENSIONS OFF PARENT_SCOPE) # Turn off gnu extensions
   get_property(all_features GLOBAL PROPERTY CMAKE_CXX_KNOWN_FEATURES)
   if ("${PARSED_RELEASE_WHITELIST}" STREQUAL "")
     set(release_features ${all_features})
